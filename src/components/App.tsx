a808d017838d61daefa4f039f8cdcf96e3304029
Bent Focken <bent.focken@pm.me>
2021-09-30 22:41:51 +0200

    feat: use the redux store's data to fill the `App` component with categories

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,29 @@
-import BaseList from './BaseList';
 import React, {Component, ReactNode} from 'react';
+import {connect, ConnectedProps} from 'react-redux';
+import {RootState} from '../redux/store';
+import Category from './Category';
 
-export class App extends Component {
+class App extends Component<AppProps> {
     public render(): ReactNode {
         return (
             <div>
                 <h1>Unge Notes</h1>
-                <BaseList />
+                {
+                    this.props.categories.map((c) => <Category key={c.title} category={c} />)
+                }
             </div>
         );
     }
 }
+
+const connector = connect(
+    (state: RootState) => {
+        return {
+            categories: state.data.categories ?? [],
+        };
+    },
+);
+
+type AppProps = ConnectedProps<typeof connector>;
+
+export default connector(App);
