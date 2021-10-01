bb16b2804b505a95e5a683b601a6ad4d77f80ace
Bent Focken <bent.focken@pm.me>
2021-10-01 12:37:57 +0200

    chore: add `scheduleSave` method to limit amount of write operations
    Use it instead of a direct `save` call after setting data.
    Moreover, make a deep copy of the given `partial` data in the `setPartial` method to avoid reference and redux problems.

--- src/util/Store.ts
+++ src/util/Store.ts
@@ -4,11 +4,16 @@ import * as path from 'path';
 
 export class Store<T extends {[key: string]: any} = {}> {
     private static instances: {[name: string]: Store} = {};
+    private static TIMEOUT_IN_S = 5;
 
-    private path: string;
+    private changeTimeout: NodeJS.Timeout;
+    private readonly name: string;
+    private readonly path: string;
     private data: T;
 
     private constructor(filename: string) {
+        this.name = filename;
+
         if (!filename.endsWith('.json')) {
             filename += '.json';
         }
@@ -40,22 +45,51 @@ export class Store<T extends {[key: string]: any} = {}> {
 
     public set(key: keyof T, value: any): void {
         this.data[key] = value;
-        this.save();
+        this.scheduleSave();
     }
 
     public setPartial(partial: Partial<T>): void {
         this.data = {
             ...this.data,
-            ...partial,
+            ...JSON.parse(JSON.stringify(partial)),
         };
-        this.save();
+        this.scheduleSave();
     }
 
     public get<S>(key: keyof T): S {
         return this.data[key];
     }
 
-    private save() {
+    /**
+     * Schedules the save operation to be executed in TIMEOUT_IN_S
+     * seconds. If already a timeout exists, it is cleared and
+     * another one is started.
+     *
+     * This reduces the number of save calls and therefore write
+     * operations to the hard drive drastically, as otherwise
+     * every keystroke would be saved in a separate write operation.
+     * @private
+     */
+    private scheduleSave(): void {
+        if (this.changeTimeout) {
+            // remove old timeout
+            clearTimeout(this.changeTimeout);
+        }
+
+        // set timeout
+        this.changeTimeout = setTimeout(
+            () => {
+                this.save();
+            },
+            Store.TIMEOUT_IN_S*1000,
+        );
+    }
+
+    /**
+     * Saves the store to the corresponding file, synchronously.
+     * @private
+     */
+    private save(): void {
         fs.writeFileSync(this.path, JSON.stringify(this.data));
     }
 }
