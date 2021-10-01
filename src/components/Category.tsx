35d3dbd9915fff405333d1da81ae0490a1ad77e1
Bent Focken <bent.focken@pm.me>
2021-10-01 12:45:47 +0200

    chore: add `add note` button to Category component

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -3,14 +3,30 @@ import {connect, ConnectedProps} from 'react-redux';
 import {RootState} from '../redux/store';
 import classNames from 'classnames';
 import Note from './Note';
+import {addNoteToCategory} from '../redux/dataSlice';
+import {NoteHelper} from '../util/NoteHelper';
 
 class Category extends Component<CategoryProps> {
     public render() {
         return (
-            <div className={classNames('category')}>
+            <div
+                className={classNames('category')}
+                onKeyDown={(evt) => {
+                    if (evt.key === 'n' && evt.ctrlKey) {
+                        this.props.addNote();
+                    }
+                }}
+            >
+                <h2>{this.props.category.title}</h2>
                 {
                     (this.props.category.notes ?? []).map((n) => <Note key={n.id} note={n}/>)
                 }
+                <div
+                    className={classNames('add-note')}
+                    onClick={this.props.addNote}
+                >
+                    <button>Add note</button>
+                </div>
             </div>
         );
     }
@@ -20,6 +36,11 @@ const connector = connect(
     (state: RootState, ownProps: {category: ICategory}) => {
         return {
             category: ownProps.category,
+        };
+    },
+    (dispatch, ownProps) => {
+        return {
+            addNote: () => dispatch(addNoteToCategory({note: {id: NoteHelper.getNewId(), title: '', dateCreated: (new Date()).toJSON(), lastModified: (new Date()).toJSON()}, category: ownProps.category})),
         };
     },
 );
