0e30f056a13b969f8a66d056811d516c18791285
Jenny <code@nordglueck.com>
2021-10-16 20:08:08 +0200

    chore: import tailwind in renderer

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -29,6 +29,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
+// eslint-disable-next-line import/no-unresolved
+import './tailwind.output.css';
 import App from './components/App';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
