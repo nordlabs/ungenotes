bd53c102e85544787e8bc2f950ab5ba0eb117330
befocken <bent.focken@pm.me>
2022-04-10 16:51:55 +0100

    chore: integrate sidebar width better

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import {Route, Routes } from 'react-router-dom';
 import CategoryRoute from '../routes/CategoryRoute';
+import {useAppSelector} from '../util/hooks';
 
 export default function App(): JSX.Element {
+    const sidebarOpened = useAppSelector(state => state.data.sidebarOpened);
+
     return (
         <div>
             <Sidebar />
-            <div className={'ml-64'}>
+            <div className={sidebarOpened ? 'ml-64' : 'ml-16'} style={{transition: 'all 0.5s ease'}}>
                 <Routes>
                     <Route path={'category'}>
                         <Route path={':categoryId'} element={<CategoryRoute />} />
